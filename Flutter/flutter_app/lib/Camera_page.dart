af0f188f1cd64fea7f74a471ce63a5a456079142
hope-john <36391218+hope-john@users.noreply.github.com>
2020-08-20 19:08:03 +0700

    Test Model

--- Flutter/flutter_app/lib/Camera_page.dart
+++ Flutter/flutter_app/lib/Camera_page.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-
+import 'package:tflite/tflite.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' show join;
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter/services.dart';
+
+
 
 class MyHomePage extends StatefulWidget {
   @override
