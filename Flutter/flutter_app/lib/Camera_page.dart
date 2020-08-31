2aeceaa5977ecb9853a62858b3cd691a767fe968
hope-john <36391218+hope-john@users.noreply.github.com>
2020-08-31 17:38:07 +0700

    Update Camera_page.dart

--- Flutter/flutter_app/lib/Camera_page.dart
+++ Flutter/flutter_app/lib/Camera_page.dart
@@ -10,8 +10,6 @@ import 'package:dio/dio.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/services.dart';
 
-
-
 class MyImagePicker extends StatefulWidget {
   @override
   MyImagePickerState createState() => MyImagePickerState();
@@ -37,75 +35,77 @@ class MyImagePickerState extends State {
     });
   }
 
- void classifyImage(BuildContext context) async {
-   FormData formData = new FormData.fromMap({
-      "file": await MultipartFile.fromFile(path)
-   });
-   try {
-      var response = await Dio().post("http://localhost:5000/upload", data: formData); 
+  void classifyImage(BuildContext context) async {
+    FormData formData =
+        new FormData.fromMap({"file": await MultipartFile.fromFile(path)});
+    try {
+      var response =
+          await Dio().post("http://localhost:5000/upload", data: formData);
       var parsedJson = json.decode(response.data.toString());
       setState(() {
-        result = "${parsedJson['class']}\n${parsedJson['score'].toStringAsFixed(2)} %";
+        result =
+            "${parsedJson['class']}\n${parsedJson['score'].toStringAsFixed(2)} %\nCal: ${parsedJson['cal']}";
       });
-   } catch (e) {
-     setState(() {
+    } catch (e) {
+      setState(() {
         result = e.toString();
       });
-   }
- }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-          width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-        image: DecorationImage(
-        image: AssetImage('assets/food_bg2.jpg'),
-        fit: BoxFit.cover,
-         ),
-       ),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  imageURI == null
-                      ? Text('No image selected',style:TextStyle(color: Colors.orangeAccent,fontSize: 24))
-
-                      : Image.file(imageURI, width: 224, height: 224, fit: BoxFit.cover),
-                  Container(
-                      margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
-                      child: RaisedButton(
-                        onPressed: () => getImageFromCamera(),
-                        child: Text('Select Image From Camera'),
-                        textColor: Colors.white,
-                        color: Colors.orangeAccent,
-                        padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
-                      )),
-                  Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                      child: RaisedButton(
-                        onPressed: () => getImageFromGallery(),
-                        child: Text('Select Image From Gallery'),
-                        textColor: Colors.white,
-                        color: Colors.orangeAccent,
-                        padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
-                      )),
-                  Container(
-                      margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
-                      child: RaisedButton(
-                        onPressed: () => classifyImage(context),
-                        child: Text('Classify Image'),
-                        textColor: Colors.white,
-                        color: Colors.orangeAccent,
-                        padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
-                      )),
-                  result == null
-                      ? Text('Result',style:TextStyle(color: Colors.orangeAccent,fontSize: 18))
-                      : Text(result,style:TextStyle(color: Colors.orangeAccent))
-                      
-                ]))
-    );
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/food_bg2.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: <
+                    Widget>[
+              imageURI == null
+                  ? Text('No image selected',
+                      style:
+                          TextStyle(color: Colors.orangeAccent, fontSize: 24))
+                  : Image.file(imageURI,
+                      width: 224, height: 224, fit: BoxFit.cover),
+              Container(
+                  margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                  child: RaisedButton(
+                    onPressed: () => getImageFromCamera(),
+                    child: Text('Select Image From Camera'),
+                    textColor: Colors.white,
+                    color: Colors.orangeAccent,
+                    padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                  )),
+              Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: RaisedButton(
+                    onPressed: () => getImageFromGallery(),
+                    child: Text('Select Image From Gallery'),
+                    textColor: Colors.white,
+                    color: Colors.orangeAccent,
+                    padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                  )),
+              Container(
+                  margin: EdgeInsets.fromLTRB(0, 30, 0, 20),
+                  child: RaisedButton(
+                    onPressed: () => classifyImage(context),
+                    child: Text('Classify Image'),
+                    textColor: Colors.white,
+                    color: Colors.orangeAccent,
+                    padding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                  )),
+              result == null
+                  ? Text('Result',
+                      style:
+                          TextStyle(color: Colors.orangeAccent, fontSize: 18))
+                  : Text(result, style: TextStyle(color: Colors.orangeAccent,fontSize: 18))
+            ])));
   }
-
 }
