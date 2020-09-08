9046f7cb695abda9babc56e1f40c571e6916dd7f
hope-john <36391218+hope-john@users.noreply.github.com>
2020-09-08 16:49:20 +0700

    Update History page

--- Flutter/flutter_app/lib/History.dart
+++ Flutter/flutter_app/lib/History.dart
@@ -12,71 +12,34 @@ class HistoryPage extends StatefulWidget {
 }
 
 class HistoryPageState extends State<HistoryPage> {
-  final InfiniteScrollController _infiniteController = InfiniteScrollController(
-    initialScrollOffset: 0.0,
-  );
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 1,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('History'),
-          backgroundColor: Colors.orangeAccent,
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.arrow_downward),
-              onPressed: () {
-                _infiniteController.animateTo(
-                    _infiniteController.offset + 2000.0,
-                    duration: const Duration(milliseconds: 250),
-                    curve: Curves.easeIn);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("History"),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: Center(
+        child: FutureBuilder(
+          builder: (context, snapshot) {
+            var showData = json.decode(snapshot.data.toString());
+            return ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  onTap: () {},
+                  
+                  title: Text(showData[index]['timestamp']),
+                  subtitle: Text(showData[index]['department']),
+                  trailing: const Icon(Icons.chevron_right),
+                  
+                );
               },
-            ),
-            IconButton(
-              icon: const Icon(Icons.arrow_upward),
-              onPressed: () {
-                _infiniteController.animateTo(
-                    _infiniteController.offset - 2000.0,
-                    duration: const Duration(milliseconds: 250),
-                    curve: Curves.easeIn);
-              },
-            ),
-          ],
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(text: ''),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            _buildTab(0),
-          ],
+              itemCount: showData.length,
+            );
+          },
+          future: DefaultAssetBundle.of(context).loadString("assets/food.json"),
         ),
       ),
-    );
-  }
-
-  Widget _buildTab(int tab) {
-    return InfiniteListView.separated(
-      key: PageStorageKey(tab),
-      controller: _infiniteController,
-      itemBuilder: (BuildContext context, int index) {
-        return Material(
-          child: InkWell(
-            onTap: () {},
-            child: ListTile(
-              title: Text('Tab $tab Item #$index'),
-              subtitle: Text('Subtitle $index'),
-              trailing: const Icon(Icons.chevron_right),
-            ),
-          ),
-        );
-      },
-      separatorBuilder: (BuildContext context, int index) =>
-          const Divider(height: 2.0),
-      anchor: 0.5,
     );
   }
 }
